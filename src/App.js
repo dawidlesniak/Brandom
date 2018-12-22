51a0b7542d9d7855d3c88a9695dd0281dc2e10ab
dawidlesniak <dawlesniak@gmail.com>
2018-12-22 20:03:44 +0100

    Added sounds and ticking function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,13 +9,14 @@ class App extends Component {
     this.handleMaxChange = this.handleMaxChange.bind(this);
     this.calculateTime = this.calculateTime.bind(this);
     this.startCountDown = this.startCountDown.bind(this);
+    this.startClock = this.startClock.bind(this);
+    this.stopClock = this.stopClock.bind(this);
     this.tick = this.tick.bind(this);
 
     this.state = {
       minValue: 10,
       maxValue: 60,
-      seconds: '00',
-      minutes: ''
+      isRunning: false
     };
 
   }
@@ -37,12 +38,35 @@ class App extends Component {
 
     var rand = Math.floor(Math.random() * (max - min)) + min
     console.log(rand);
+    return rand;
   }
 
   startCountDown() {
+    if (this.state.isRunning) {
+      console.log('stopping');
+      this.stopClock();
+    } else {
+      console.log('starting');
+      this.startClock();
+    }
+  }
+
+  startClock() {
+    this.setState({isRunning: true});    
     this.intervalHandle = setInterval(this.tick, 1000);
-    let time = this.state.minutes;
-    this.secondsRemaining = time * 60;
+    // let time = this.state.minutes;
+    this.secondsRemaining = this.calculateTime();
+    this.tickSound = new Audio('tick.mp3');
+    
+  }
+
+  stopClock(withAlarm) {
+    this.setState({isRunning: false});
+    clearInterval(this.intervalHandle);
+    if (withAlarm) {
+      console.log('ring!');
+    }
+    console.log('end');
   }
 
   render() {
@@ -50,7 +74,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Random Timer</p>
+          <p>Random Timer - BRandom!</p>
           {/*  */}
           {/* Timer */}
           <label>
@@ -62,34 +86,57 @@ class App extends Component {
           <input onChange={this.handleMaxChange} type="number" name="name" />
           </label>
 
-          <a onClick={this.startCountDown} >Start</a>
+          <button onClick={this.startCountDown} >Start</button>
         </header>
       </div>
     );
   }
 
-  tick() {
-    var min = Math.floor(this.secondsRemaining / 60);
-    var sec = this.secondsRemaining - (min * 60);
-    this.setState({
-      minutes: min,
-      seconds: sec
-    })
-    if (sec < 10) {
-      this.setState({
-        seconds: "0" + this.state.seconds,
-      })
-    }
-    if (min < 10) {
-      this.setState({
-        value: "0" + min,
-      })
-    }
-    if (min === 0 & sec === 0) {
-      clearInterval(this.intervalHandle);
-    }
-    this.secondsRemaining--
+  tick() {    
+    if (this.secondsRemaining <= 0) {
+      this.stopClock(true);
+      var audio = new Audio('kaboom.mp3');
+      audio.play();
+      // clearInterval(this.intervalHandle);      
+    } else {
+      this.tickSound.play();
+      this.secondsRemaining--
+      console.log(this.secondsRemaining);
+    }    
   }
+
+
+  // foo() {
+  //   var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
+
+  //   // Update the count down every 1 second
+  //   var x = setInterval(function () {
+
+  //     // Get todays date and time
+  //     var now = new Date().getTime();
+
+  //     // Find the distance between now and the count down date
+  //     var distance = countDownDate - now;
+
+  //     // Time calculations for days, hours, minutes and seconds
+  //     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  //     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  //     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  //     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  //     // Output the result in an element with id="demo"
+  //     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  //       + minutes + "m " + seconds + "s ";
+
+  //     // If the count down is over, write some text 
+  //     if (distance < 0) {
+  //       clearInterval(x);
+  //       document.getElementById("demo").innerHTML = "EXPIRED";
+  //     }
+  //   }, 1000);
+  // }
+
 }
+
 
 export default App;
